baa75573ee5ae1fd5e03f8f815db1df6500e4f49
Glitch (denim-mirror-heliotrope) <none>
2024-11-11 00:06:55 +0000

    Updated navigation framework to be able to go back to Home page

--- src/pages/payroll.jsx
+++ src/pages/payroll.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Navbar from "../components/navbar";
+import "../styles/styles.css"; 
 
 export default function Payroll() {
   return (
-    <div>
-      <h1>Payroll Page</h1>
-      <p>Welcome to the payroll page!</p>
+    <div className="container">
+      <Navbar /> {/* Sidebar navigation */}
+      
+      <div className="pay-container">
+        <h1>Payroll Page</h1>
+        <p>Welcome to the payroll page!</p>
+      </div>
     </div>
   );
 }
--- src/pages/timetracking.jsx
+++ src/pages/timetracking.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Navbar from "../components/navbar";
+import "../styles/styles.css"; 
 
 export default function TimeTracking() {
   return (
-    <div>
-      <h1>Time Tracking Page</h1>
-      <p>Welcome to the time tracking page!</p>
+    <div className="container">
+      <Navbar /> {/* Sidebar navigation */}
+      
+      <div className="time-container">
+        <h1>Time Tracking Page</h1>
+        <p>Welcome to the time tracking page!</p>
+      </div>
     </div>
   );
 }
