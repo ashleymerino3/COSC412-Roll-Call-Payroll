e96054fb6a5f219cc6c6b996d8946aaf6629504e
Glitch (salty-scientific-iberis) <none>
2024-11-20 02:14:53 +0000

    WORKING COMMIT #2 -ASHLEYYYY

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,7 +4,7 @@ import Stopwatch from "../components/stopwatch";
 import "../styles/styles.css"; 
 
 export default function Home() {
-  const hello = "Roll Call Payroll System";
+  const hello = "Roll Call Payroll System ";
 
   return (
     <div className="container">
@@ -12,7 +12,7 @@ export default function Home() {
 
       <div className="home-container">
         <h1 className="title">{hello}</h1>
-        <p>Welcome Back User!</p>
+        <p>Welcome Back User (ALICEEEEE)!</p>
         <div className = "stopwatch-container">
         <Stopwatch /> {/* Add the Stopwatch component here */}
         </div>
